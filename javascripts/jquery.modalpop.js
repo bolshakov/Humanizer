f813ca77857283fc9bf481db0107838b64684e13
TweeKane <tweekane@gmail.com>
2011-07-26 12:57:19 +0400

    Modal window js code imporovement.

--- javascripts/jquery.modalpop.js
+++ javascripts/jquery.modalpop.js
@@ -1,47 +1,47 @@
 //ModalPop
 //Author: Owain Lewis
 //Author URL: www.Owainlewis.com
-//Simple Modal Dialog for jQuery
+//Simple Modal Dialog for $
 //The idea here was to keep this plugin as lightweight and easy to customize as possible
 //You are free to use this plugin for whatever you want.
 //If you enjoy this plugin, I"d love to hear from you
 
 (function() {
 
-jQuery.fn.modalpop = function(options) {
+$.fn.center = function () {
+  this.css("position","absolute");
+  this.css("top", (($(window).height() - this.outerHeight()) / 2) + $(window).scrollTop() + "px");
+  this.css("left", (($(window).width() - this.outerWidth()) / 2) + $(window).scrollLeft() + "px");
+  return this;
+}
+
+$.fn.modalpop = function(options) {
 
 	var defaults = {
-		speed: 500,
-		center: false
+		speed: 500
 	};
 
 	var options = $.extend(defaults, options);
 	var width = $(window).width();
 	//Get the full page height including the scroll area
 	var height = $(document).height();
-	jQuery("body").prepend("<div id=\"overlay\"></div>");
-	jQuery("#overlay").css("height", height);
-	jQuery("#overlay").css("width", width);
+	$("body").prepend("<div id=\"overlay\"></div>");
+	$("#overlay").css("height", height);
+	$("#overlay").css("width", width);
 
 	return this.each(function() {
-    var id = jQuery(this).attr("href");
+	  var selcector = $(this).attr("href");
+    var id = $(selcector);
 
-		jQuery(this).click(function() {
-			//Set the popup window to center if required
-			if (defaults.center == true) {
-				$(id).css("top",  height/2-$(id).height()/2);
-			} else {
-				$(id).css("top",  200);
-			}
-			$(id).css("left", width/2-$(id).width()/2);
-			jQuery("#overlay").fadeIn(options.speed);
-			jQuery(id).fadeIn(options.speed);
+		$(this).click(function() {
+		  id.center();
+			$("#overlay, " + selcector).fadeIn(options.speed);
 			return false;
 		});
 
-		jQuery(".close, #overlay").click(function(){
-			jQuery(id).fadeOut(options.speed, function() {
-			  jQuery("#overlay").toggle();
+		$(".close, #overlay").click(function(){
+			id.fadeOut(options.speed, function() {
+			  $("#overlay").toggle();
 			});
 			return false;
 		});
@@ -49,5 +49,5 @@ jQuery.fn.modalpop = function(options) {
 	});
 };
 
-})(jQuery);
+})($);
 
